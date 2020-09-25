64e20cd909b9b749a89019e01b5ffa7a43b54188
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2020-09-26 00:36:15 +0530

    scrollable table added

--- src/app/shared/widgets/table/table.component.ts
+++ src/app/shared/widgets/table/table.component.ts
@@ -7,26 +7,38 @@ import {Component} from '@angular/core';
 })
 export class TableComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['trade_id', 'trade_dt', 'trade_type', 'trader', 'security', 'security_type', 
+                                  'quantity', 'price'];
   dataSource = ELEMENT_DATA;
 }
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  trade_id: number;
+  trade_dt: string;
+  trade_type: string;
+  trader: string;
+  security: string;
+  security_type: string;
+  quantity: number;
+  price: number
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
+  {trade_id: 1, trade_dt: '10-23-04', trade_type: 'sell', trader: 'Citi', security: 'Facebook', security_type: 'Equity Shares', quantity: 1000, price: 8222},
 ];
